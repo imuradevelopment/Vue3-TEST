f022f36ab915f990fa011b740d872cab2ad1a4f7
imuradevelopment <imuradevelopmentauth@gmail.com>
2023-09-21 16:40:51 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import DefinePropsComponent from './components/DefinePropsComponent.vue'
+
+</script>
+
+<template id="app">
+    <main>
+        <ul>
+            <li>
+                <h2>テンプレート構文</h2>
+            </li>
+            <DefinePropsComponent msg="テストメッセージ1" />
+        </ul>
+    </main>
+</template>
+
+<style scoped>
+    h2 {
+        color:var(--vue-green);
+    }
+</style>
---
+++ src/components/DefinePropsComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="col">
+        <div>
+            <div>
+                データバインディングで最も基本の形式は、「マスタッシュ構文」(二重中括弧) によるテキスト展開です
+            </div>
+            <a href="https://ja.vuejs.org/guide/essentials/template-syntax.html#text-interpolation">
+                テキスト展開
+            </a>
+        </div>
+        <div>
+            {{ $options.__name }}のテスト：
+            <span class="test">{{ msg }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{
+    msg: string
+}>()
+
+</script>
+
+<style scoped>
+.col {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+}
+
+.col > div:first-child {
+    margin: 0 20px 0 0;
+    border-right: dashed 1px var(--vue-green);
+}
+
+.test {
+    color: var(--vue-green);
+}
+</style>
